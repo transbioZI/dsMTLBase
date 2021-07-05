f09f5b612cb9ddfb5d205c3a7746891f947183ba
Hank <hank9cao@gmail.com>
2021-07-05 20:54:07 +0200

    change upload function

--- inst/uploadFuntions.R
+++ inst/uploadFuntions.R
@@ -7,31 +7,31 @@ opals=c(opal.login(url = "http://192.168.56.100:8080/", user = "administrator", 
 #note: set func=null. 
 ####
 #calculate variables' updates per iterations 
-dsadmin.set_method(opal = opals, name = "LS_iter_updateDS", func=NULL, path = "./dsMTLBase/LS_iter_updateDS.R", type = "aggregate")
-dsadmin.set_method(opal = opals, name = "LR_iter_updateDS", func=NULL, path = "./dsMTLBase/LR_iter_updateDS.R", type = "aggregate")
-dsadmin.set_method(opal = opals, name = "LS_funcVal_evalDS", func=NULL, path = "./dsMTLBase/LS_funcVal_evalDS.R", type = "aggregate") 
-dsadmin.set_method(opal = opals, name = "LR_funcVal_evalDS", func=NULL, path = "./dsMTLBase/LR_funcVal_evalDS.R", type = "aggregate") 
-dsadmin.set_method(opal = opals, name = "updateHDS", func=NULL, path = "./dsMTLBase/updateHDS.R", type = "aggregate") #for inmf
-dsadmin.set_method(opal = opals, name = "updateOtherMatsDS", func=NULL, path = "./dsMTLBase/updateOtherMatsDS.R", type = "assign") #for inmf
+dsadmin.set_method(opal = opals, name = "LS_iter_updateDS", func=NULL, path = "./R/LS_iter_updateDS.R", type = "aggregate")
+dsadmin.set_method(opal = opals, name = "LR_iter_updateDS", func=NULL, path = "./R/LR_iter_updateDS.R", type = "aggregate")
+dsadmin.set_method(opal = opals, name = "LS_funcVal_evalDS", func=NULL, path = "./R/LS_funcVal_evalDS.R", type = "aggregate") 
+dsadmin.set_method(opal = opals, name = "LR_funcVal_evalDS", func=NULL, path = "./R/LR_funcVal_evalDS.R", type = "aggregate") 
+dsadmin.set_method(opal = opals, name = "updateHDS", func=NULL, path = "./R/updateHDS.R", type = "aggregate") #for inmf
+dsadmin.set_method(opal = opals, name = "updateOtherMatsDS", func=NULL, path = "./R/updateOtherMatsDS.R", type = "assign") #for inmf
 
 #initialize the matrix
-dsadmin.set_method(opal = opals, name = "addInterceptDS", func=NULL, path = "./dsMTLBase/addInterceptDS.R", type = "assign") # for lasso and mtl
-dsadmin.set_method(opal = opals, name = "initMatricesDS", func=NULL, path = "./dsMTLBase/initMatricesDS.R", type = "assign") # for inmf
-dsadmin.set_method(opal = opals, name = "initRanMatsWithSeedsDS", func=NULL, path = "./dsMTLBase/initRanMatsWithSeedsDS.R", type = "assign") #inmf
+dsadmin.set_method(opal = opals, name = "addInterceptDS", func=NULL, path = "./R/addInterceptDS.R", type = "assign") # for lasso and mtl
+dsadmin.set_method(opal = opals, name = "initMatricesDS", func=NULL, path = "./R/initMatricesDS.R", type = "assign") # for inmf
+dsadmin.set_method(opal = opals, name = "initRanMatsWithSeedsDS", func=NULL, path = "./R/initRanMatsWithSeedsDS.R", type = "assign") #inmf
 
 
 #calculate mean squared error and miss classification rate
-dsadmin.set_method(opal = opals, name = "calcMSEDS", func=NULL, path = "./dsMTLBase/calcMSEDS.R", type = "aggregate")
-dsadmin.set_method(opal = opals, name = "calcMCRDS", func=NULL, path = "./dsMTLBase/calcMCRDS.R", type = "aggregate")
+dsadmin.set_method(opal = opals, name = "calcMSEDS", func=NULL, path = "./R/calcMSEDS.R", type = "aggregate")
+dsadmin.set_method(opal = opals, name = "calcMCRDS", func=NULL, path = "./R/calcMCRDS.R", type = "aggregate")
 
 
 #matrix calculate t(X)%*%Y 
-dsadmin.set_method(opal = opals, name = "xtyDS", func=NULL, path = "./dsMTLBase/xtyDS.R", type = "aggregate")
+dsadmin.set_method(opal = opals, name = "xtyDS", func=NULL, path = "./R/xtyDS.R", type = "aggregate")
 
 #get/set my data with key
-dsadmin.set_method(opal = opals, name = "getMyServerDataDS", func=NULL, path = "./dsMTLBase/getMyServerDataDS.R", type = "aggregate") 
-dsadmin.set_method(opal = opals, name = "setMyServerDataDS", func=NULL, path = "./dsMTLBase/setMyServerDataDS.R", type = "assign") 
-dsadmin.set_method(opal = opals, name = "subsetSubjestsDS", func=NULL, path = "./dsMTLBase/subsetSubjestsDS.R", type = "assign") 
+dsadmin.set_method(opal = opals, name = "getMyServerDataDS", func=NULL, path = "./R/getMyServerDataDS.R", type = "aggregate") 
+dsadmin.set_method(opal = opals, name = "setMyServerDataDS", func=NULL, path = "./R/setMyServerDataDS.R", type = "assign") 
+dsadmin.set_method(opal = opals, name = "subsetSubjestsDS", func=NULL, path = "./R/subsetSubjestsDS.R", type = "assign") 
 
 
 
